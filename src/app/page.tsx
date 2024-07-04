98759df8492b1d133f90933f10251001a225d34d
404Nikhil <nicksd111@gmail.com>
2024-07-05 02:27:33 +0530

    connect embed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { ConnectEmbed } from "@/app/thirdweb";
+import { client } from "./client";
+import { chain } from "./chain";
 
 export default function Home() {
   return (
    <>
-   
+       <div className="flex flex-col items-center justify-start h-[100vh] mt-8">
+      <h1 className="mb-4">Buy me a coffee</h1>
+      <ConnectEmbed 
+        client={client}
+        chain={chain}
+      />
+    </div>
    </>
   );
 }
